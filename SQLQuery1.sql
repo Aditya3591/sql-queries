81ea1a619ede4527861631899993f39610d4b58f
Aditya3591 <adityasinghi12345@gmail.com>
2024-03-04 12:53:01 +0530

    [ADITYA] added: performed all join operations

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -10,6 +10,13 @@ create table department_info
 	insert into department_info values('3591','calfornia','software developer');
 	insert into department_info values('1844','usa','analyst');
 	insert into department_info values('7747','calfornia','marketing');
+	insert into department_info values('4565','cannada','designer');
 	select * from department_info;
 
 	select em_id,emp_name,emp_salary,dept_location,dept_name from emplyoee_info inner join department_info on emplyoee_info.deptID=department_info.emp_dept_id;
+
+	select em_id,emp_name,emp_salary,dept_location,dept_name from emplyoee_info left outer join department_info on emplyoee_info.deptID=department_info.emp_dept_id;
+	
+	select em_id,emp_name,emp_salary,dept_location,dept_name from emplyoee_info right outer join department_info on emplyoee_info.deptID=department_info.emp_dept_id;
+
+	select em_id,emp_name,emp_salary,dept_location,dept_name from emplyoee_info full outer join department_info on emplyoee_info.deptID=department_info.emp_dept_id;
