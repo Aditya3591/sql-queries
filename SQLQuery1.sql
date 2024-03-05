1d446907428c13501d50d9bb2ca3e5f8e73f5902
Aditya3591 <adityasinghi12345@gmail.com>
2024-03-05 11:49:40 +0530

    [aditya] added:if avg salary > than 30000

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -10,7 +10,19 @@ select @name =emp_name,@salary=emp_salary from emplyoee_info where deptID =@depi
  select @name as name,@salary as salary;
  end
  go
- 
 
+ begin
+ declare @salary decimal;
+ select @salary=avg(emp_salary) from emplyoee_info;
+ select @salary as 'avg salary'
 
+ if @salary>30000
+ begin
+ print 'avg salary is greater thn 30000';
+ end
+ else
+ begin
+ print 'avg salary is less than 30000';
+ end
+ end
 
