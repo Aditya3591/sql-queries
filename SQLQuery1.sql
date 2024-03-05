a4e2b8678241b82667670478feb641f51cc857cb
Aditya3591 <adityasinghi12345@gmail.com>
2024-03-05 17:40:23 +0530

    [aditya] added:creating a view for join

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,9 +1,9 @@
-alter procedure proc_emplyoee_details
-as
-begin
-select em_id,emp_name,dept_location,dept_name from emplyoee_info inner join department_info on emp_dept_id=deptID 
+create view v_emplyoee_details_based_on_location 
+as select em_id,emp_name,dept_location,dept_name from emplyoee_info inner join department_info on emp_dept_id=deptID
 
-end
-go
 
-execute proc_emplyoee_details;
+select * from v_emplyoee_details_based_on_location;
+
+
+
+sp_helptext v_emplyoee_details_based_on_location;
