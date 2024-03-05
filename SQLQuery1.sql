abc7272e8af517b1aea502f8027cfa0f04be3d3b
Aditya3591 <adityasinghi12345@gmail.com>
2024-03-05 15:27:55 +0530

    [aditya] added:concept of time and delay

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,63 +1,14 @@
+select getdate() as 'time';
+go
 
-
-
-
-begin
-
-declare @name varchar(20),@salary int,@depid varchar (20)='1844';
+begin 
+waitfor time '15:22:00'
 select * from emplyoee_info;
-select @name =emp_name,@salary=emp_salary from emplyoee_info where deptID =@depid;
- select @name as name,@salary as salary;
- end
- go
+end
+go
 
- begin
- declare @salary decimal;
- select @salary=avg(emp_salary) from emplyoee_info;
- select @salary as 'avg salary'
-
- if @salary>30000
- begin
- print 'avg salary is greater thn 30000';
- end
- else
- begin
- print 'avg salary is less than 30000';
- end
- end
- go
-
-
- select * from emplyoee_info
- begin
-	 while (select min(emp_salary) from emplyoee_info)<900000
-	 begin
-	 update emplyoee_info
-	 set emp_salary+=1000;
-	 print 'salary updated';
-	 select * from emplyoee_info
-
-	 if (select min(emp_salary) from emplyoee_info)>=900000
-	 begin
-	 print 'the avg salary is greater than 80000'
-	 break
-	 end
-
-	 end
-	end
- go
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+begin 
+waitfor delay '00:00:30';
+select * from emplyoee_info;
+end
+go
