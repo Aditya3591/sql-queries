8d421d2955fa2d6e977a72754d3c234536c374c5
Aditya3591 <adityasinghi12345@gmail.com>
2024-03-05 16:32:00 +0530

    [aditya] added:check the emp details through location

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,10 +1,14 @@
-create procedure proc_emplyoee_details
+
+select * from department_info;
+go
+
+create procedure pro_emplyoee_info_location_wise(@loaction varchar(20))
 
 as
-
 begin
-select * from emplyoee_info;
+
+select * from emplyoee_info inner join department_info on emp_dept_id=deptID where dept_location=@loaction;
 end
 go
 
-execute proc_emplyoee_details;
+execute pro_emplyoee_info_location_wise @loaction='calfornia'
