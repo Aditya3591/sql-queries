50f6534f766ce4953545acd98071dcbd4bda9f4b
Aditya3591 <adityasinghi12345@gmail.com>
2024-03-05 16:51:42 +0530

    [aditya] added:some changes in prcedure

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,14 +1,9 @@
-
-select * from department_info;
-go
-
-create procedure pro_emplyoee_info_location_wise(@loaction varchar(20))
-
+alter procedure proc_emplyoee_details
 as
 begin
+select em_id,emp_name,dept_location,dept_name from emplyoee_info inner join department_info on emp_dept_id=deptID 
 
-select * from emplyoee_info inner join department_info on emp_dept_id=deptID where dept_location=@loaction;
 end
 go
 
-execute pro_emplyoee_info_location_wise @loaction='calfornia'
+execute proc_emplyoee_details;
